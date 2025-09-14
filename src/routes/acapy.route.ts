d40952411dc0c646acd1568501bddd284e9ec93c
sagarmetaverse <sagar.karmoker@bracu.ac.bd>
2025-09-14 14:27:05 +0600

    feat: add endpoints to fetch created schemas and schema by ID, and create credential definitions

--- src/routes/acapy.route.ts
+++ src/routes/acapy.route.ts
@@ -65,4 +65,63 @@ router.post('/schemas', async (req, res) => {
     }
 })
 
+/**
+ * get all created schemas
+ * GET /schemas 
+ * methods also available http://localhost:8021/api/doc#/schemas/get_schemas_created
+ */
+
+router.get('/schemas', async (req, res) => {
+    try {
+        const response = await axiosInstance.get('/schemas/created');
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch schemas from ACA-Py' });
+    }
+})
+
+/**
+ * get schema by schema_id
+ * http://localhost:3000/api/v1/schemas/HrtRSHvELgrURjujcYXWVr:2:UniID:1.0
+ * methods also available http://localhost:8021/api/doc#/schemas/get_schema
+*/
+router.get('/schemas/:schema_id', async (req, res) => {
+    try {
+        const { schema_id } = req.params;
+        const response = await axiosInstance.get(`/schemas/${schema_id}`);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch schema from ACA-Py' });
+    }
+})
+
+
+/**
+ * create credential definition
+ * its represent a specific instance of a schema that an issuer can use to issue credentials.
+ * tags can be different: v1, v2, default, student, employee, etc.
+ * post body example
+ * {
+    "schema_id": "HrtRSHvELgrURjujcYXWVr:2:UniID:1.0",
+    "support_revocation": false,
+    "tag": "default"
+    }
+ */
+
+router.post('/credential-definitions', async (req, res) => {
+    try {
+        const { schema_id, support_revocation, tag } = req.body;
+        if (!schema_id || typeof support_revocation !== 'boolean' || !tag) {
+            return res.status(400).json({ error: 'Missing required fields: schema_id, support_revocation (boolean), tag' });
+        }
+        const credentialDefinitionData = { schema_id, support_revocation, tag };
+        const response = await axiosInstance.post('/credential-definitions', credentialDefinitionData);
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to create credential definition in ACA-Py' });
+    }
+});
+
+
 export default router;
